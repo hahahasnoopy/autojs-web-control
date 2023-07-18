d3ac881dc3c95611d079770676ab3eedb245573e
lei.mao <lei.mao@ly.com>
2023-07-18 17:14:08 +0800

    fix path

--- web/vue.config.js
+++ web/vue.config.js
@@ -25,7 +25,7 @@ module.exports = {
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
   publicPath: '/',
-  outputDir: '../public',
+  outputDir: '../server/modules/www',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
